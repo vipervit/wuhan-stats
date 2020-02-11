00fc4d861f093499e4743e251049703c49613205
Viper Vit <vitolg1@gmail.com>
2020-02-11 11:13:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     license='Apache',
     description='Simple desktop alert for Wuhan virus statistics updates',
     packages = find_packages(),
-    version='1.13.8'
+    version='1.13.9'
 )
