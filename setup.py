90b8106824ecf2348848b85ac2e5ff3096b53130
Viper Vit <vitolg1@gmail.com>
2020-02-12 10:54:36 -0500

    1.15.09: Upodate setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from wuhan_stats import __version__
 setup(
-    name = 'wuhan-stats',
+    name = 'wuhan_stats',
     author='vipervit',
     author_email='vitolg1@gmail.com',
     license='Apache',
