bca661e62ec8b0879350b918e765992aa8dbe2f3
Viper Vit <vitolg1@gmail.com>
2020-02-12 09:29:22 -0500

    1.15.04: merge strategy corrected

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     license='Apache',
     description='Simple desktop alert for Wuhan virus statistics updates',
     packages = find_packages(),
-    version='1.15.03'
+    version='1.15.04'
 )
