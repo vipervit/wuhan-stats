6dd18bbc6afcb32e78470f150888783ba11c9db9
Viper Vit <vitolg1@gmail.com>
2020-02-14 18:45:16 -0500

    1.19.03: Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from wuhan_stats import __version__
 
 with open("README.md", "r") as f:
-    long_description = fh.read()
+    long_description = f.read()
 
 setup(
     name = 'wuhan_stats',
