483cbfa0921849c451596740d944d82a62b99262
Viper Vit <vitolg1@gmail.com>
2020-02-19 12:24:16 -0500

    1.4.2: pyinstaller Mac executables added

--- wuhan_stats.py
+++
@@ -1,3 +0,0 @@
-from wuhan_stats.__main__ import main
-
-main()
--- wuhan_stats/__init__.py
+++ wuhan_stats/__init__.py
@@ -4,4 +4,4 @@ logger = logging.getLogger(__name__)
 console = logging.StreamHandler()
 logger.addHandler(console)
 
-__version__ = '1.4.1'
+__version__ = '1.4.2'
