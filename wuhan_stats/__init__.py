9e2bccf0dccef84afb0c8175b0611984e10a0b4f
Viper Vit <vitolg1@gmail.com>
2020-03-10 13:04:55 -0400

    1.7.2: email alert added

--- wuhan_stats/__init__.py
+++ wuhan_stats/__init__.py
@@ -4,4 +4,10 @@ logger = logging.getLogger(__name__)
 console = logging.StreamHandler()
 logger.addHandler(console)
 
-__version__ = '1.6.1'
+__version__ = '1.7.2'
+
+SLEEP = 3600
+SLEEP_MIN = 900
+WIN_NOTIFICATION_TIMEOUT = 3600
+
+SITES = { 'Worldometer': {'home': 'https://www.worldometers.info', 'updates': '/coronavirus/'} } # Other sources may be added in the future
