b3611738092066b742bf5f51202bf4c2aea64a62
Viper Vit <vitolg1@gmail.com>
2020-03-12 22:36:11 -0400

    1.7.5: counter added, email checkbox added

--- wuhan_stats/__init__.py
+++ wuhan_stats/__init__.py
@@ -4,7 +4,7 @@ logger = logging.getLogger(__name__)
 console = logging.StreamHandler()
 logger.addHandler(console)
 
-__version__ = '1.7.4'
+__version__ = '1.7.5'
 
 PERIOD_DEFAULT = 1 # hours
 PERIOD_MIN = 0.25 # hours
--- wuhan_stats/src/app.py
+++ wuhan_stats/src/app.py
@@ -15,18 +15,27 @@ class Application(Frame):
             worldometer.get()
             if prev < worldometer.last_updated:
                 worldometer.desktop.send()
-                worldometer.email.send()
+                if self._checkboxval.get() == 1:
+                    worldometer.email.send()
                 prev = worldometer.last_updated
             if not __debug__:
                 logger.debug('Exiting due to debug mode.')
                 sys.exit()
 
     def __set_interval__(self):
-        self._interval = int(self.pollinterval.get()) * 3600000
+        reading = int(self.PollingInterval.get())
+        self._interval = reading * 3600000
+
+    def __run_counter__(self):
+        if self.Start["text"] == "Stop":
+            self.Counter["text"] = str(self._elapsed)
+            self._elapsed -= 1
+            self.master.after(1000, self.__run_counter__)
 
     def run(self):
         if self.Start["text"] == "Stop":
-            self.__set_interval__()
+            self._elapsed = int(self.PollingInterval.get()) * 3600
+            self.__run_counter__()
             self.execute()
             self.master.after(self._interval, self.run)
 
@@ -35,26 +44,26 @@ class Application(Frame):
             self.Start["text"] = "Stop"
         else:
             self.Start["text"] = "Start"
+            self.Counter["text"] = "0"
+        self.__set_interval__()
         self.run()
 
     def createWidgets(self):
 
-        self.text = Label(self, text="Polling interval (hrs):")
-        self.text.pack(side=LEFT)
-
-        self.pollinterval = Spinbox(self, from_=1, to=24, increment=1, width=5)
-        self.pollinterval.pack(side=LEFT)
-
-        self.Start = Button(self, text="Start", command=self.startstop_change_title)
+        self.SendEmail = Checkbutton(text='Email    ', variable=self._checkboxval).pack(side=LEFT)
+        self.Text = Label(text="Polling(hrs):").pack(side=LEFT)
+        self.PollingInterval = Spinbox(from_=1, to=24, increment=1, width=5)
+        self.PollingInterval.pack(side=LEFT)
+        self.Counter = Button(text="0", width=4)
+        self.Counter.pack(side=LEFT)
+        self.Start = Button(text="Start", command=self.startstop_change_title)
         self.Start.pack(side=LEFT)
-
-        self.QUIT = Button(text="QUIT", command=self.quit, fg="red")
-        self.QUIT.pack(side=RIGHT)
-
-
+        self.QUIT = Button(text="QUIT", command=self.quit, fg="red").pack(side=RIGHT)
 
     def __init__(self, master=None):
         self._interval = None
+        self._elapsed = 0
+        self._checkboxval = IntVar()
         Frame.__init__(self, master)
         self.createWidgets()
         self.pack()
